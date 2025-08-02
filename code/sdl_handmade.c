12ce55180a5f2c193c5cc425e003e332e0726fe1
Maximilian Shah <ChameleonOnATwig@gmail.com>
2025-08-02 18:42:20 -0400

    Handmade Penguin Chapter 5: Graphics Refactoring and Review

--- code/sdl_handmade.c
+++ code/sdl_handmade.c
@@ -1,76 +1,94 @@
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_video.h>
 #include <stdbool.h>
 #include <sys/mman.h>
 
-static SDL_Texture *gp_texture;
-static void *gp_bitmap_memory;
-static int g_bitmap_width;
-static int g_bitmap_height;
-static int g_bytes_per_pixel = 4;
+struct SDL_Offscreen_Buffer {
+	// NOTE: Pixels are always 32-bits wide, Memory Order: BB GG RR XX
+	SDL_Texture *p_texture;
+	void *p_memory;
+	int width;
+	int height;
+	int pitch;
+};
 
-static void SDLResizeTexture(SDL_Renderer *renderer, int width, int height) {
-	if (gp_bitmap_memory) {
-		munmap(gp_bitmap_memory,
-		 g_bitmap_width * g_bitmap_height * g_bytes_per_pixel);
+struct SDL_Window_Dimension {
+	int width;
+	int height;
+};
+
+static struct SDL_Offscreen_Buffer g_backbuffer;
+
+static struct SDL_Window_Dimension SDLGetWindowDimension(SDL_Window *p_window) {
+	struct SDL_Window_Dimension result;
+	SDL_GetWindowSize(p_window, &result.width, &result.height);
+	return result;
+}
+
+static void SDLResizeTexture(struct SDL_Offscreen_Buffer *p_buffer, SDL_Renderer *p_renderer, int width, int height) {
+	const int bytes_per_pixel = 4;
+
+	if (p_buffer->p_memory) {
+		munmap(p_buffer->p_memory,
+		 p_buffer->width * p_buffer->height * bytes_per_pixel);
 	}
-	if (gp_texture) { SDL_DestroyTexture(gp_texture); }
+	if (p_buffer->p_texture) { SDL_DestroyTexture(p_buffer->p_texture); }
 
-	gp_texture = SDL_CreateTexture(renderer,
+	p_buffer->p_texture = SDL_CreateTexture(p_renderer,
 							 SDL_PIXELFORMAT_ARGB8888,
 							 SDL_TEXTUREACCESS_STREAMING,
 							 width,
 							 height);
 
-	g_bitmap_width = width;
-	g_bitmap_height = height;
-	gp_bitmap_memory = mmap(0,
-						 width * height * g_bytes_per_pixel,
+	p_buffer->width = width;
+	p_buffer->height = height;
+	p_buffer->pitch = width * bytes_per_pixel;
+	p_buffer->p_memory = mmap(0,
+						 width * height * bytes_per_pixel,
 						 PROT_READ | PROT_WRITE,
 						 MAP_ANONYMOUS | MAP_PRIVATE,
 						 -1,
 						 0);
 }
 
-static void SDLUpdateWindow(SDL_Window *window, SDL_Renderer *renderer) {
-	SDL_UpdateTexture(gp_texture,
+static void SDLUpdateWindow(SDL_Window *p_window, SDL_Renderer *p_renderer, struct SDL_Offscreen_Buffer buffer) {
+	SDL_UpdateTexture(buffer.p_texture,
 				   0,
-				   gp_bitmap_memory,
-				   g_bitmap_width * g_bytes_per_pixel);
+				   buffer.p_memory,
+				   buffer.pitch);
 
-	SDL_RenderCopy(renderer,
-				gp_texture,
+	SDL_RenderCopy(p_renderer,
+				buffer.p_texture,
 				0,
 				0);
 
-	SDL_RenderPresent(renderer);
+	SDL_RenderPresent(p_renderer);
 }
 
-bool HandleEvent(SDL_Event *event) {
+bool HandleEvent(SDL_Event *p_event) {
 	bool should_quit = false;
 
-	switch(event->type)	{
+	switch(p_event->type)	{
 		case SDL_QUIT: {
 			printf("SDL_QUIT\n");
 			should_quit = true;
 		} break;
 
 		case SDL_WINDOWEVENT: {
-			switch(event->window.event) {
+			switch(p_event->window.event) {
 				case SDL_WINDOWEVENT_SIZE_CHANGED: {
-					SDL_Window *window = SDL_GetWindowFromID(event->window.windowID);
-					SDL_Renderer *renderer = SDL_GetRenderer(window);
+					SDL_Window *p_window = SDL_GetWindowFromID(p_event->window.windowID);
+					SDL_Renderer *p_renderer = SDL_GetRenderer(p_window);
 
-					printf("SDL_WINDOWEVENT_SIZE_CHANGED (%d, %d)\n", event->window.data1, event->window.data2);
+					printf("SDL_WINDOWEVENT_SIZE_CHANGED (%d, %d)\n", p_event->window.data1, p_event->window.data2);
 
-					SDLResizeTexture(renderer, event->window.data1, event->window.data2);
+					SDLResizeTexture(&g_backbuffer, p_renderer, p_event->window.data1, p_event->window.data2);
 				} break;
 
 				case SDL_WINDOWEVENT_EXPOSED: {
-					SDL_Window *window = SDL_GetWindowFromID(event->window.windowID);
-					SDL_Renderer *renderer = SDL_GetRenderer(window);
+					SDL_Window *p_window = SDL_GetWindowFromID(p_event->window.windowID);
+					SDL_Renderer *p_renderer = SDL_GetRenderer(p_window);
 
-					SDLUpdateWindow(window, renderer);
+					SDLUpdateWindow(p_window, p_renderer, g_backbuffer);
 				} break;
 			}
 		} break;
@@ -86,21 +104,21 @@ int main(int argc, char *argv[]) {
 						  0);
 
 	// Create a Window
-	SDL_Window *window = SDL_CreateWindow("Handmade Hero",
+	SDL_Window *p_window = SDL_CreateWindow("Handmade Hero",
 									   SDL_WINDOWPOS_UNDEFINED,
 									   SDL_WINDOWPOS_UNDEFINED,
 									   640,
 									   480,
 									   SDL_WINDOW_RESIZABLE);
 
-	if (window) {
+	if (p_window) {
 		// Create a Renderer for the Window
-		SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
+		SDL_Renderer *p_renderer = SDL_CreateRenderer(p_window, -1, 0);
 
-		if (renderer) {
+		if (p_renderer) {
 			int width, height;
-			SDL_GetWindowSize(window, &width, &height);
-			SDLResizeTexture(renderer, width, height);
+			SDLGetWindowDimension(p_window);
+			SDLResizeTexture(&g_backbuffer, p_renderer, width, height);
 
 			bool game_running = true;
 			while(game_running) {
@@ -110,7 +128,7 @@ int main(int argc, char *argv[]) {
 						game_running = false;
 					}
 				}
-				SDLUpdateWindow(window, renderer);
+				SDLUpdateWindow(p_window, p_renderer, g_backbuffer);
 			}
 		}
 	}
